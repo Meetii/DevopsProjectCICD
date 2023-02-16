502833ef7ad76828c7cf2da435ee8b62b65a740e
Muhamet Osmani <muhamet.osmani@it-labs.com>
2023-02-16 21:16:18 +0100

    remove unnecessary comments

--- src/main/java/com/example/devops/DevopsApplication.java
+++ src/main/java/com/example/devops/DevopsApplication.java
@@ -6,13 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevopsApplication {
 
-//    echo "# DevopsProjectCICD" >> README.md
-//    git init
-//    git add README.md
-//    git commit -m "first commit"
-//    git branch -M master
-//    git remote add origin https://github.com/Meetii/DevopsProjectCICD.git
-//    git push -u origin master
     public static void main(String[] args) {
         SpringApplication.run(DevopsApplication.class, args);
     }
